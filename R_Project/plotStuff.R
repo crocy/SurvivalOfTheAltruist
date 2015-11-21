3dc9bfa767471f4dc739ff042be989aa6aa81e20
Nejc Krokter <teh.croc@gmail.com>
2015-11-21 23:49:18 +0100

    Updated plotStuff.R

--- R_Project/plotStuff.R
+++ R_Project/plotStuff.R
@@ -4,12 +4,18 @@ plotAltruismToLifetime <- function(data) {
   qplot(altruism, lifetime, data = data, color = generator)
 }
 
+dataAll = loadCsvData()
+
 qplot(lifetime, data = dataAll, color = groupTag, binwidth = 5)
 qplot(lifetime, data = dataAll, fill = generator, size = groupTag, binwidth = 5)
 qplot(lifetime, data = dataAll, color = groupTag, geom = "density")
 qplot(altruism, lifetime, data = dataAll, color = groupTag, geom = "density2d")
 qplot(altruism, lifetime, data = dataAll, color = groupTag, geom = "smooth")
-qplot(altruism, lifetime, data = dataAll, color = generator, geom = "smooth")
 qplot(altruism, lifetime, data = dataAll, geom = "smooth")
 qplot(altruism, lifetime, data = dataAll, color = generator, geom = c("smooth", "point"))
 qplot(altruism, lifetime, data = dataAll, color = generator, geom = c("smooth", "point"), size = energyShared)
+qplot(altruism, lifetime, data = dataAll, facets = . ~ generator, geom = "density2d", color = generator)
+
+qplot(altruism, lifetime, data = dataAll, color = generator, geom = "smooth")
+qplot(altruism, lifetime, data = dataAll, color = generator, geom = "smooth", shape = groupTag)
+qplot(altruism, lifetime, data = dataAll, fill = generator, geom = "smooth", shape = groupTag)
